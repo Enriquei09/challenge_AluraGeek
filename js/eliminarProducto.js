89a812fcac624f041a5f30262af5ef7abd746a63
Enriquei09 <kike1299.car@gmail.com>
2024-12-16 02:22:08 -0600

    update

--- js/eliminarProducto.js
+++ js/eliminarProducto.js
@@ -6,7 +6,7 @@ document.addEventListener("click", async (event) => {
         const id = deleteButton.getAttribute("data-id");
         try {
             await conexionApi.eliminarProducto(id);
-            console.log(`Producto con ID ${id} eliminado`);
+            alert(`Producto con ID ${id} eliminado`);
             // Eliminar la tarjeta del DOM
             deleteButton.closest("li").remove();
         } catch (error) {
