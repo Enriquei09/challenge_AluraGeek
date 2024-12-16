ba30250cb722736934582795e6d7a9fafdfd479e
Enriquei09 <kike1299.car@gmail.com>
2024-12-16 01:45:23 -0600

    create cleaninputs function

--- js/registrarProducto.js
+++ js/registrarProducto.js
@@ -1,5 +1,6 @@
 import { conexionApi } from "./conexionApi.js";
 const form = document.querySelector("[data-form]");
+const btnClean = document.querySelector("#Bclean");
 
 async function creaProduct(evento){
     evento.preventDefault();
@@ -11,4 +12,11 @@ async function creaProduct(evento){
     window.location.href="./challengeAluraGeek.html";
 }
 
+async function inputsClean(){
+    document.querySelector("[data-name]").value="";
+    document.querySelector("[data-price]").value="";
+    document.querySelector("[data-urlImage]").value="";
+}
+
 form.addEventListener("submit", evento => creaProduct(evento));
+btnClean.addEventListener("click",inputsClean);
