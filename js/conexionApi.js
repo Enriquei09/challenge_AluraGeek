ed8a8978b2a013767750f4b18a4a7e2f928eae1e
Enriquei09 <kike1299.car@gmail.com>
2024-12-16 01:30:13 -0600

    update Post method

--- js/conexionApi.js
+++ js/conexionApi.js
@@ -9,10 +9,15 @@ async function listarProductos(){
 }
 
 async function addProduct(name,price,urlImage){
+
+    const ultimoId = await obtenerUltimoId(); // Obtener el último ID registrado
+    const nuevoId = ultimoId + 1; // Calcular el nuevo ID
+
     const conexion = await fetch("http://localhost:3000/productos",{
         method: "POST",
         headers:{"Content-type":"application/json"},
         body:JSON.stringify({
+            id: nuevoId.toString(),
             name:name,
             price:price,
             urlImage:urlImage
@@ -23,8 +28,14 @@ async function addProduct(name,price,urlImage){
     return conxexionConvertida
 }
 
+async function obtenerUltimoId() {
+    const productos = await listarProductos();
+    if (productos.length === 0) return 0; // Si no hay productos, el primer ID será 1
+    return Math.max(...productos.map(producto => parseInt(producto.id))); // Encuentra el mayor ID
+}
+
 export const conexionApi={
-    listarProductos,addProduct
+    listarProductos,addProduct,obtenerUltimoId
 }
 
 //listarProductos();
